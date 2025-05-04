76b710dec4459eac4ddcf5c190c689abd392cbb1
judi2641 <judi2641@bht-berlin.de>
2025-05-04 20:47:13 +0200

    neue interfaces für DataTransfer eingeführt

---
+++ src/endpoints/publicUsers/UserTypes.ts
@@ -0,0 +1,21 @@
+export interface IUser{
+    userID: string;
+    firstName?: string;
+    lastName?: string;
+    password: string;
+    isAdministrator: boolean;
+}
+
+export interface IUserDocument extends IUser, Document{
+    toSafeJSON(): TransferUser;
+}
+
+export interface TransferUser{
+    userID: string;
+    firstName?: string;
+    lastName?: string;
+    isAdministrator: boolean;
+    id: string;
+    accessToken: string;
+    refreshToken: string;
+}

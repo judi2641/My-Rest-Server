a3a03b9c852c2b391f2887638800f05c992f144a
judi2641 <judi2641@bht-berlin.de>
2025-05-04 22:20:51 +0200

    UserTypes in UserModel eingefügt

--- src/endpoints/users/UserModel.ts
+++ src/endpoints/users/UserModel.ts
@@ -1,6 +1,34 @@
 import mongoose from "mongoose";
 import bcryptjs from 'bcryptjs';
-import { IUser, IUserDocument, TransferUser } from "./UserTypes";
+
+/**
+ * interface für den user wie er in der datenbank liegt
+ */
+export interface IUser{
+    userID: string;
+    firstName?: string;
+    lastName?: string;
+    password: string;
+    isAdministrator: boolean;
+}
+/**
+ * interface zum umgang mit dem user in der API. braucht methode, die den user safe zum transfer macht
+ */
+export interface IUserDocument extends IUser, Document{
+    toSafeJSON(): TransferUser;
+    toJSON(): IUser;
+}
+
+/**
+ * interface zum transfer des users. beitzt zusätzliche felder für token, aber keins für das passwort
+ */
+export interface TransferUser{
+    userID: string;
+    firstName?: string;
+    lastName?: string;
+    isAdministrator: boolean;
+    id: string;
+}
 
 const UserSchema = new mongoose.Schema<IUserDocument>({
     userID: {type: String, required: true, unique: true, immutable: true},

8dd94925a61631b623485a69d5e8f0977a48d3e1
judi2641 <judi2641@bht-berlin.de>
2025-05-04 17:13:36 +0200

    add AuthenticationRoute

--- src/HttpServer.ts
+++ src/HttpServer.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { initDB } from './database/db';
 import PublicUserRoute from './endpoints/user/PublicUserRoute';
+import AuthenticationRoute from './endpoints/authentication/AuthenticationRoute';
 import { initAdministrator } from './endpoints/user/UserService';
 
 
@@ -9,6 +10,7 @@ const port = 80;
 
 app.use(express.json());
 app.use('/api/publicUsers',PublicUserRoute);
+app.use('/api/authenticate', AuthenticationRoute);
 app.use((req: Request, res: Response) => {
     res.status(404).json({ error: "Route not found"})
 });

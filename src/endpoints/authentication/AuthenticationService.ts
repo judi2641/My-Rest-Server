d1397af20d6929ef1ad62d98924e1abfff81aae9
judi2641 <judi2641@bht-berlin.de>
2025-05-19 20:20:37 +0200

    little changes

--- src/endpoints/authentication/AuthenticationService.ts
+++ src/endpoints/authentication/AuthenticationService.ts
@@ -10,26 +10,32 @@ import config from 'config';
  * @returns safeUser object and token if authentication was sucessfull
  */
 export async function authenticate(userID: string, password: string){
-    const user = await getUserByUserID(userID);
-    const checkCredentials = await bcryptjs.compare(password, user.password);
+    try{
+        const user = await getUserByUserID(userID);
+        const checkCredentials = await bcryptjs.compare(password, user.password);
 
-    if(!checkCredentials) {
-        console.log("authentication failed")
+        if(!checkCredentials) {
+            console.log("authentication failed")
+            throw new HttpError(401, 'Failed to create a token: Authentication failed');
+        }
+        console.log("authentication sucessfull -- creating a token");
+
+        const token = jwt.sign(
+            {
+                userID: user.userID,
+                isAdministrator: user.isAdministrator
+            },
+            config.get<string>('session.tokenKey'),
+            {
+                algorithm: 'HS256',
+                expiresIn: '1h'
+            }
+        )
+        const safeUser = user.toSafeJSON();
+        return {safeUser, token};
+    }
+    catch(error){
+        console.log(error);
         throw new HttpError(401, 'Failed to create a token: Authentication failed');
     }
-    console.log("authentication sucessfull -- creating a token");
-
-    const token = jwt.sign(
-        {
-            userID: user.userID,
-            isAdministrator: user.isAdministrator
-        },
-        config.get<string>('session.tokenKey'),
-        {
-            algorithm: 'HS256',
-            expiresIn: '1h'
-        }
-    )
-    const safeUser = user.toSafeJSON();
-    return {safeUser, token};
 };
